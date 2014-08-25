dd1a180c6bdd6409c06901f7d72f27116e44b0f9
Ryan McVeigh <ryanm@newmediadenver.com>
2014-08-25 08:44:07 -0600

    added new functionality for outputing lengths (in pixels) added snap to grid based on grid size in admin form, and started the transform functionality.

--- testfiles/js/custom/testone.js
+++ testfiles/js/custom/testone.js
@@ -31,10 +31,6 @@
     return(Math.round(num / multiple) * multiple);
   }
 
-  //styling on canvas
-  $('#drawing').css({
-  });
-
   //show admin form
   $(document).on('click', '#admin-form-link', function(event) {
     event.preventDefault();
@@ -53,7 +49,11 @@
 
   var svgElement = document.getElementById("drawing");
   var s = Snap(svgElement);
+
+  //style canvas
   s.attr({ viewBox: "0 0 "+canvasWidth100 +" "+ canvasHeight100});
+
+  //style div
   $('#drawing').css({
       width: canvasWidthPx,
       height: canvasHeightPx,
@@ -61,10 +61,10 @@
       border: 'solid #aaa 1px'
     });
 
-  //draw outer wall
+  //draw outer wall (function below)
   buildOuterwall("outerWall");
 
-  //drag limitor plugin
+  //drag plugin
   (function() {
 
     Snap.plugin( function( Snap, Element, Paper, global ) {
@@ -108,26 +108,120 @@
     });
   })();
 
+  Snap.plugin( function( Snap, Element, Paper, global ) {
 
+    Element.prototype.addHandles = function() {
+    var dragging = 0;
+    var handleGroup;
+
+    var startResize = function() {
+            this.data('origTransform', this.transform().local);
+    }
+
+    var moveResize = function(dx,dy) {
+            var scale = 1 + dx / 50;
+            this.attr({
+                    transform: this.data('origTransform') + (this.data('origTransform') ? "S" : "s") + scale
+            });
+    }
+
+    var stopResize = function() {};
+
+    var bb = this.getBBox();
+
+    var x= bb.x,
+      y=bb.y,
+      x2 = bb.x2,
+      y2=bb.y2,
+      centerX=bb.cx,
+      centerY=bb.cy,
+      handle = new Array();
+
+    var currentDrawerWidth = $userForm.find('#drawer-width').val(),
+      currentDrawerDepth = $userForm.find('#drawer-depth').val(),
+      currentDrawerWidth100 = currentDrawerWidth*100,
+      currentDrawerDepth100= currentDrawerDepth*100;
+
+    var halfDrawerWidth = currentDrawerWidth100/2;
+
+    var halfDrawerDepth = currentDrawerDepth100/2;
+
+    var quarterDrawerDepth = currentDrawerDepth100/4;
+
+    var minxDragPoint = 0-(currentDrawerWidth100*0.25),
+      minyDragPoint = 0-halfDrawerDepth,
+      maxxDragPoint = currentDrawerWidth100-(currentDrawerWidth100*0.25),
+      maxyDragPoint = currentDrawerDepth100-halfDrawerDepth;
+
+    var ftOption = {
+      handleFill: "silver",
+      handleStrokeDash: "5,5",
+      handleStrokeWidth: "2",
+      handleLength: "75",
+      handleRadius: "7",
+      handleLineWidth: 2,
+    };
+
+    handle[0] = s.circle(x, y, ftOption.handleRadius).attr({class: 'handler', fill: ftOption.handleFill});
+    handle[1] = s.circle(x2, y2, ftOption.handleRadius).attr({class: 'handler', fill: ftOption.handleFill});
+    handle[2] = s.circle(centerX, centerY, ftOption.handleRadius).attr({class: 'handler', fill: ftOption.handleFill});
+    handleGroup = s.group(handle[0], handle[1], handle[2]);
+    // handleGroup.drag();
+    // handleGroup.limitDrag({ x: 0, y: 0, minx: minxDragPoint, miny: minyDragPoint, maxx: maxxDragPoint, maxy: maxyDragPoint});
+    };
+
+  })
+
+  // opens dialog then clears the canvas
   $('#clear-btn').click(function(event) {
     event.preventDefault();
-    s.clear();
-    //add and remove selected class
-    $('.template-image-link').each(function() {
-      var $thisLink = $(this);
-      if ($thisLink.hasClass('selected')) {
-        $thisLink.removeClass('selected')
-      };
-    });
-    //draw outer wall
-    buildOuterwall("outerWall");
+    $( "#clear-canvas-dialog" ).dialog( "open" );
   });
+
 
   var drawerWidth100 = drawerWidth*100;
   var drawerDepth100= drawerDepth*100;
 
-  //function that outputs Template
-  function getTemplate(jsonFile) {
+  //function that outputs Template (NON-FLEXIBLY) from orig json file.
+  function getFixedTemplate(jsonFile){
+    var jsonPath = "json/"+jsonFile+"Length.json",
+    currentDrawerWidth = $userForm.find('#drawer-width').val(),
+    currentDrawerDepth = $userForm.find('#drawer-depth').val(),
+    currentDrawerWidth100 = currentDrawerWidth*100,
+    currentDrawerDepth100= currentDrawerDepth*100;
+
+    $.getJSON(jsonPath,function(result){
+      $.each(result.paths, function(i, path){
+        var x1 = path.x1,
+          y1 = path.y1,
+          x2 = path.x2,
+          y2 = path.y2;
+        var p = s.path(
+          "M "+x1+" "+y1+" L " +x2+ " "+y2
+        ).attr({
+          stroke: "#000",
+          strokeWidth: 4,
+          "fill-opacity": "0"
+        });
+        if (x1==x2) {
+          var minxDragPoint = 0-x1,
+            minyDragPoint = 0-y1,
+            maxxDragPoint = currentDrawerWidth100-x2,
+            maxyDragPoint = currentDrawerDepth100-y1;
+          p.limitDrag({ x: 0, y: 0, minx: minxDragPoint, miny: minyDragPoint, maxx: maxxDragPoint, maxy: maxyDragPoint});
+        }else{
+          var minxDragPoint = 0-x1,
+            minyDragPoint = 0-y1,
+            maxxDragPoint = currentDrawerWidth100-x1,
+            maxyDragPoint = currentDrawerDepth100-y2;
+          p.limitDrag({ x: 0, y: 0, minx: minxDragPoint, miny: minyDragPoint, maxx: maxxDragPoint, maxy: maxyDragPoint});
+        }
+      });
+    });
+  };
+
+  //function that outputs Template FLEXIBLY from orig jSon File.
+  function getFlexableTemplate(jsonFile) {
     var jsonPath = "json/"+jsonFile+".json",
     currentDrawerWidth = $userForm.find('#drawer-width').val(),
     currentDrawerDepth = $userForm.find('#drawer-depth').val(),
@@ -165,7 +259,7 @@
   }
 
   //build outer wall (could change to hard code if json can't be pulled in)
-    //function that outputs Template
+  //function that outputs Template where VECTORS ARE NOT DRAGGABLE!
   function buildOuterwall(jsonFile) {
     var jsonPath = "json/"+jsonFile+".json",
     currentDrawerWidth = $userForm.find('#drawer-width').val(),
@@ -189,6 +283,30 @@
       });
     });
   }
+
+  //template image link.
+  //get json based on click.
+  // $(document).on('click', '.template-image-link', function(event) {
+  //   event.preventDefault();
+
+  //   var thisId = $(this).attr('id'),
+  //     $clickedLink = $(this);
+
+  //   //add and remove selected class
+  //   $('.template-image-link').each(function() {
+  //     var $thisLink = $(this);
+  //     if ($thisLink.hasClass('selected')) {
+  //       $thisLink.removeClass('selected')
+  //     };
+  //   }).promise().done(function(){
+  //     $clickedLink.addClass('selected');
+  //   });
+
+  //   //open dialog
+  //   $( "#template-change-dialog" ).dialog( "open" );
+
+  // });
+
   $(document).on('click', '.template-image-link', function(event) {
     event.preventDefault();
 
@@ -209,7 +327,7 @@
     s.clear();
 
     //get json and draw
-    getTemplate(thisId);
+    getFlexableTemplate(thisId);
 
   });
 
@@ -242,13 +360,13 @@
       if ($(this).hasClass('selected')) {
         var thisId = $(this).attr('id');
         //get json and draw
-        getTemplate(thisId);
+        getFlexableTemplate(thisId);
       };
     });
 
   });
 
-  //create vertical line
+  //create horizontal line
   $('#horizontal-line').click(function(event) {
     var currentDrawerWidth = $userForm.find('#drawer-width').val(),
       currentDrawerDepth = $userForm.find('#drawer-depth').val(),
@@ -268,8 +386,10 @@
         strokeWidth: 4
     });
     p.limitDrag({ x: 0, y: 0, minx: minxDragPoint, miny: minyDragPoint, maxx: maxxDragPoint, maxy: maxyDragPoint});
+    // p.addHandles()
   });
 
+  //create vertical line
   $('#vertical-line').click(function(event) {
     var currentDrawerWidth = $userForm.find('#drawer-width').val(),
       currentDrawerDepth = $userForm.find('#drawer-depth').val(),
@@ -326,5 +446,62 @@
     var json='{ "paths": [ ' + points.replace(/\}{/g, '}, {') + " ] }";
     alert("Just saved this json: "+json);
   });
+
+  //create template dialog
+  $( "#template-change-dialog" ).dialog({
+      resizable: false,
+      autoOpen: false,
+      height:420,
+      width:520,
+      modal: true,
+      buttons: {
+        "Fit into drawer": function() {
+          //clear canvas
+          s.clear();
+          var thisId = $('.template-image-link.selected').attr('id');
+          //get json and draw
+          getFlexableTemplate(thisId);
+
+          $( this ).dialog( "close" );
+        },
+        "Place above": function() {
+          //clear canvas
+          s.clear();
+          var thisId = $('.template-image-link.selected').attr('id');
+          //get json and draw
+          getFixedTemplate(thisId);
+
+          $( this ).dialog( "close" );
+        }
+      }
+    });
+
+  //create clear dialog
+  $( "#clear-canvas-dialog" ).dialog({
+      resizable: false,
+      autoOpen: false,
+      height:420,
+      width:520,
+      modal: true,
+      buttons: {
+        "Yes": function() {
+          s.clear();
+          //add and remove selected class
+          $('.template-image-link').each(function() {
+            var $thisLink = $(this);
+            if ($thisLink.hasClass('selected')) {
+              $thisLink.removeClass('selected')
+            };
+          });
+          //draw outer wall
+          buildOuterwall("outerWall");
+
+          $( this ).dialog( "close" );
+        },
+        "Cancel": function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    });
 
 }(jQuery));
